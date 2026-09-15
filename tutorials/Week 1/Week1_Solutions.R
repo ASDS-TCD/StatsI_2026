c2678c6814199a7e1589dd250d77db21dc2ba86d
ethanzeh <ethanzeh21@gmail.com>
2026-09-15 14:55:20 +0100

    week 1 finished solutions

--- tutorials/Week 1/Week1_Solutions.R
+++ tutorials/Week 1/Week1_Solutions.R
@@ -23,7 +23,7 @@ print(sum(y))
 # now do the same thing, but faster using the built-in R function
 find_mean <- function(l) {
   sum <- 0
-  for(i in 1:length(l)){
+  for(i in seq_along(l)){
     sum <- sum + l[i]
   }
   sum / length(l)
@@ -33,8 +33,26 @@ print(find_mean(y))
 print(mean)
 
 # (3) find sum of demeaned values
+demean_values <- function(l, mean){
+  ans <- NULL
+  for(i in seq_along(l)){
+    ans[i] <- l[i] - mean
+  }
+  ans
+}
+
+demeaned_y <- demean_values(y, find_mean(y))
+print(demeaned_y)
+
+# built-in way (R is so weird)
+print(y - mean(y))
 
 # (4) calculate sum of squared error
+
+# this is ridiculous. list * list..?
+squared_errors <- demeaned_y * demeaned_y
+sum_of_squared_errors <- sum(squared_errors)
+print(sum_of_squared_errors)
 
 ###########
 # Quantiles
@@ -43,11 +61,19 @@ print(mean)
 # create vector
 quantilesVec <- c(55, 84, 65, 54, 61, 67, 80, 59, 81, 82)
 
-# (1) calculate median 
+# (1) calculate median
+quantilesVec_median <- median(quantilesVec)
+print(quantilesVec_median)
 
 # (2) calculate quantiles
+quantile(quantilesVec)
 
 # (3) make a histogram of state median income
 state.x77[,2]
+setwd("tutorials/Week 1")
+pdf("state_median_income_histogram.pdf")
+hist(state.x77[,2])
+dev.off()
+setwd("../../")
 
 # remember to save your plot as a pdf
